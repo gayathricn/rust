a9c60b3d27ad1c4ef2f4a3d6ddbb6b716d65e9f0
Gayathri C N <gayathrinandanan@gmail.com>
2024-07-14 20:06:09 +0300

    Remove target directory from repository

--- target/debug/deps/reverse_string-0dcd746c41a31e0e.d
+++
@@ -1,5 +0,0 @@
-/Users/gayathricn/Exercism/rust/reverse-string/target/debug/deps/reverse_string-0dcd746c41a31e0e: tests/reverse-string.rs
-
-/Users/gayathricn/Exercism/rust/reverse-string/target/debug/deps/reverse_string-0dcd746c41a31e0e.d: tests/reverse-string.rs
-
-tests/reverse-string.rs:
--- target/debug/deps/reverse_string-b2c322f7a02a3c71.d
+++
@@ -1,5 +0,0 @@
-/Users/gayathricn/Exercism/rust/reverse-string/target/debug/deps/reverse_string-b2c322f7a02a3c71: src/lib.rs
-
-/Users/gayathricn/Exercism/rust/reverse-string/target/debug/deps/reverse_string-b2c322f7a02a3c71.d: src/lib.rs
-
-src/lib.rs:
