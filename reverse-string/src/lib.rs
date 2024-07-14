d4658b810974cd23a539c6869a023a678133b02f
Gayathri C N <gayathrinandanan@gmail.com>
2024-07-14 20:11:55 +0300

    fixed reverse-string main.rs

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -2,7 +2,3 @@ pub fn reverse(input: &str) -> String {
     //todo!("Write a function to reverse {input}");
     return input.chars().rev().collect();
 }
-fn main(){
-    let st = String::new();
-    println!("Reverse of the string is {}", reverse(&st));
-}
---
+++ reverse-string/src/main.rs
@@ -0,0 +1,5 @@
+use reverse_string::reverse;
+fn main(){
+    let st = String::new();
+    println!("Reverse of the string is {}", reverse(&st));
+}
