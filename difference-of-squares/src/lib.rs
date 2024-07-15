f73210bb48574f44774041677389cd361f24f1f7
Gayathri C N <gayathrinandanan@gmail.com>
2024-07-15 20:16:55 +0300

    difference of squares program changes

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,11 +1,14 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    //todo!("square of sum of 1...{n}")
+    return ((n*(n+1))/2).pow(2);
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    //todo!("sum of squares of 1...{n}")
+    return (n*(n+1)*((2*n)+1))/6;
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    //todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    return square_of_sum(n) - sum_of_squares(n);
 }
---
+++ difference-of-squares/src/main.rs
@@ -0,0 +1,5 @@
+use difference_of_squares::difference;
+fn main(){
+    let n:u32= 0;
+    println!("The difference between square of sum and sum of squares of {} is: {}",n,difference(n));
+    }
