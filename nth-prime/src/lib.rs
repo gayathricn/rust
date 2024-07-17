be8a93073d8c7ab84f15b6c150d4debafcd2d527
Gayathri C N <gayathrinandanan@gmail.com>
2024-07-17 20:47:21 +0300

    add nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,29 @@
 pub fn nth(n: u32) -> u32 {
-    todo!("What is the 0-indexed {n}th prime number?")
+    //todo!("What is the 0-indexed {n}th prime number?")
+    if n == 0 {
+        return 2;
+    }else
+    {
+        let mut count  = 0;
+        let mut num = 1;
+        while count <= n {
+            num += 1;
+            if is_prime(num) {
+                count += 1;
+            }
+        }
+        num
+    }
+}
+fn is_prime(num: u32) -> bool {
+    if num < 2 {
+        return false;
+    }
+    let limit = (num as f64).sqrt() as u32;
+    for i in 2 ..=limit {
+        if num%i == 0 {
+            return false;
+        }
+    }
+    return true
 }
---
+++ nth-prime/src/main.rs
@@ -0,0 +1,5 @@
+use nth_prime::nth;
+fn main() {
+    let n=0;
+    println!("The {} th prime is: {}",n,nth(n));
+}
