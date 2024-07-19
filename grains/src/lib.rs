b4976eef2360cb4331967195995aab6ab4fa8e67
Gayathri C N <gayathrinandanan@gmail.com>
2024-07-19 21:49:46 +0300

    grains program changes

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,13 @@
 pub fn square(s: u32) -> u64 {
-    todo!("grains of rice on square {s}");
-}
+    //todo!("grains of rice on square {s}");
+    if s<1 || s>64 {
+        panic!("Square must be between 1 and 64");
+    }else {
+        return 2_u64.pow(s-1);
+    }
+    }
 
 pub fn total() -> u64 {
-    todo!();
+    //todo!();
+    return (1..=64).map(square).sum()
 }
---
+++ grains/src/main.rs
@@ -0,0 +1,5 @@
+use grains::{square,total};
+fn main(){
+    let s:u32= 0;
+    println!("The grains on a given square and total number of grains on the chessboard are {} and {}",square(s),total());
+    }
