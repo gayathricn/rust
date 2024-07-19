dadd36bdca5c286acb467fad77c22b1d2cc316be
Gayathri C N <gayathrinandanan@gmail.com>
2024-07-19 19:36:20 +0300

    sum of multiples program

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+//todo!("Sum the multiples of all of {factors:?} which are less than {limit}")
+let mut result = Vec::new();
+for &item in factors{
+    if item>0
+    {
+        for multiple in (item..limit).step_by(item as usize) {
+            result.push(multiple); 
+        }
+    }
+    
+}
+result.sort();
+result.dedup();
+result.iter().sum()
 }
---
+++ sum-of-multiples/src/main.rs
@@ -0,0 +1,6 @@
+use sum_of_multiples::sum_of_multiples;
+fn main() {
+    let limit: u32 = 0;
+    let factors: &[u32]= &[];
+    println!("The energy points earned by player is: {}", sum_of_multiples(limit,factors));
+}
